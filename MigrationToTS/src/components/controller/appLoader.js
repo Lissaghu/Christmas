7278c23f896a852f6b293aad4d1c7641c97bbf8c
Anton Dogadin <dogadin00@gmail.com>
2021-12-02 22:31:34 +0300

    feat: add apikey and change .prettierrc

--- MigrationToTS/src/components/controller/appLoader.js
+++ MigrationToTS/src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
-import Loader from './loader';
+import Loader from "./loader"
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super("https://newsapi.org/v2/", {
+      apiKey: "996feb5c4e8d44a0b5972ea7a1aac080",
+    })
+  }
 }
 
-export default AppLoader;
+export default AppLoader
